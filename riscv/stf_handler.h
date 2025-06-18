0b5d587288be0b1ea55282b60978fee1edfd704a
Adam Caughron <adam.caughron@condorcomputing.com>
2025-06-18 15:37:35 -0500

    Only emit necessary ForcePC records

--- riscv/stf_handler.h
+++ riscv/stf_handler.h
@@ -332,8 +332,8 @@ struct StfHandler
   
       //Optionally exclude the trace macros from the trace
       if (!include_trace_macros) {
-         stf_writer << stf::ForcePCRecord(last_npc);
-	 return;
+        _pc_record_stale = true;
+        return;
       }
     }
 
@@ -370,6 +370,11 @@ struct StfHandler
 
         bool skip_record = false;
 
+        if(_pc_record_stale) {
+          force_pc_record(PC);
+          _pc_record_stale = false;
+        }
+
         if(is_taken_branch) {
           stf_writer << stf::InstPCTargetRecord(last_npc);
         }
@@ -399,7 +404,7 @@ struct StfHandler
           ++_traced_instructions_running;
        }
     } else {
-       stf_writer << stf::ForcePCRecord(state->pc);
+       _pc_record_stale = true;
     }
   }
   // ----------------------------------------------------------------
@@ -476,7 +481,7 @@ struct StfHandler
       }
       trace_insn(p, _fetch, pc, npc, debug);
     } else {
-      force_pc_record(state->pc);
+      _pc_record_stale = true;
     }
   }
 
@@ -636,7 +641,7 @@ struct StfHandler
   void record_machine_state(processor_t *p) {
 
     auto const state = p->get_state();
-    stf_writer << stf::ForcePCRecord(state->pc);
+    _pc_record_stale = true;
 
     //TODO once dromajo comparison is done this if statement should
     //be removed, when 
@@ -985,6 +990,7 @@ private:
   uint64_t _traced_instructions_region{0};
   uint64_t _traced_instructions_running{0};
   uint64_t _traced_warmup_insns{0};
+  bool _pc_record_stale{false};
 
   static constexpr uint32_t _START_TRACE = 0x00004033; //xor x0,x0,x0
   static constexpr uint32_t _STOP_TRACE  = 0x0010c033; //xor x0,x1,x1
