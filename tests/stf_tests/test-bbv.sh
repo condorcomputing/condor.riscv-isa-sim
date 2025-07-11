5373dae04238b5db7a6d2f9c8aff4d5ce98c24c4
Adam Caughron <adam.caughron@condorcomputing.com>
2025-07-11 12:35:30 -0500

    Add usermode-only tracing to BBV unit test

--- tests/stf_tests/test-bbv.sh
+++ tests/stf_tests/test-bbv.sh
@@ -34,14 +34,46 @@ check_output() {
 	exit 1
   fi
   # Check if the output file looks right:
-  if [ ! $(grep "T:1:2 :2:10 :3:12" $BBV_OUT | wc -l) == "10" ]; then
+  if [ ! $(grep "T:1:2 :2:9 :3:12" $BBV_OUT | wc -l) == "10" ]; then
 	echo "$THIS_SCRIPT: $BBV_OUT doesn't contain the expected values."
 	echo "Spike command: "
 	echo $SPIKE_COMMAND
 	exit 1
   fi
 
-  if [ ! $(grep -v "T:1:2 :2:10 :3:12" $BBV_OUT | wc -l) == "0" ]; then
+  if [ ! $(grep -v "T:1:2 :2:9 :3:12" $BBV_OUT | wc -l) == "0" ]; then
+	echo "$THIS_SCRIPT: $BBV_OUT contains unexpected lines."
+	echo "Spike command: "
+	echo $SPIKE_COMMAND
+	exit 1
+  fi
+}
+
+check_umode_output() {
+  if [ $SPIKE_EXIT_CODE -ne 0 ]; then
+	echo "$THIS_SCRIPT: $SPIKE failed with exit code $SPIKE_EXIT_CODE"
+	echo "Spike command: "
+	echo $SPIKE_COMMAND
+	echo "Spike output: "
+	echo $SPIKE_OUTPUT
+	exit 1
+  fi
+  # Check if the output file exists
+  if [ ! -f $BBV_OUT ]; then
+	echo "$THIS_SCRIPT: $BBV_OUT not found"
+	echo "Spike command: "
+	echo $SPIKE_COMMAND
+	exit 1
+  fi
+  # Check if the output file looks right:
+  if [ ! $(grep "T:1:15" $BBV_OUT | wc -l) == "10" ]; then
+	echo "$THIS_SCRIPT: $BBV_OUT doesn't contain the expected values."
+	echo "Spike command: "
+	echo $SPIKE_COMMAND
+	exit 1
+  fi
+
+  if [ ! $(grep -v "T:1:15" $BBV_OUT | wc -l) == "0" ]; then
 	echo "$THIS_SCRIPT: $BBV_OUT contains unexpected lines."
 	echo "Spike command: "
 	echo $SPIKE_COMMAND
@@ -60,7 +92,7 @@ RISCV_BIN=$RISCV_INSTALL_DIR/$TEST_NAME.riscv
 OBJDUMP_OUT=$BASE_FILENAME.objdump
 riscv64-unknown-elf-objdump -Mnumeric -S $RISCV_BIN > $OBJDUMP_OUT
 
-SPIKE_ALL_OPTS="--isa=rv64gc_H_zba_zbb_zbc_zbs --en_bbv --bb_file $BBV_ARG --simpoint_size 24"
+SPIKE_ALL_OPTS="--isa=rv64gc_H_zba_zbb_zbc_zbs --en_bbv --bb_file $BBV_ARG --simpoint_size 23"
 export SPIKE_COMMAND="$SPIKE $SPIKE_ALL_OPTS $RISCV_BIN"
 
 
@@ -80,4 +112,27 @@ SPIKE_OUTPUT=$($SPIKE_COMMAND 2>&1 | tee "$LOG_OUT")
 SPIKE_EXIT_CODE=$?
 
 check_output
+
+
+# Umode only tracing:
+SPIKE_ALL_OPTS="--isa=rv64gc_H_zba_zbb_zbc_zbs --en_bbv --bb_file $BBV_ARG --simpoint_size 15 --bbv_umode_only"
+export SPIKE_COMMAND="$SPIKE $SPIKE_ALL_OPTS $RISCV_BIN"
+
+
+# Run Spike without STF logging:
+SPIKE_OUTPUT=$($SPIKE_COMMAND 2>&1 | tee "$LOG_OUT")
+
+SPIKE_EXIT_CODE=$?
+
+check_umode_output
+
+
+# Run Spike with STF logging:
+SPIKE_ALL_OPTS="${SPIKE_ALL_OPTS} -l"
+export SPIKE_COMMAND="$SPIKE $SPIKE_ALL_OPTS $RISCV_BIN"
+SPIKE_OUTPUT=$($SPIKE_COMMAND 2>&1 | tee "$LOG_OUT")
+
+SPIKE_EXIT_CODE=$?
+
+check_umode_output
 
