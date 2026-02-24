4ce1dd81aa1c07a9a86373f1ef2cb179af0f5456
Adam Caughron <adam.caughron@condorcomputing.com>
2026-02-24 12:40:40 -0600

    Option to disable UART input from stdin

--- riscv/sim.cc
+++ riscv/sim.cc
@@ -136,10 +136,15 @@ sim_t::sim_t(const cfg_t *cfg, bool halted,
   // that's not bus-accessible), but it should handle the normal use cases. In
   // particular, the default device tree configuration that you get without
   // setting the dtb_file argument has one.
+  std::vector<std::string> ns16550_args;
+  if (cfg->disable_stdin) {
+     ns16550_args.push_back("disable_stdin");
+  }
+
   std::vector<device_factory_sargs_t> device_factories = {
     {clint_factory, {}},
     {plic_factory, {}},
-    {ns16550_factory, {}}};
+    {ns16550_factory, ns16550_args}};
 
   // Load dtb_file if provided, otherwise self-generate a dts/dtb
   if (dtb_file) {
